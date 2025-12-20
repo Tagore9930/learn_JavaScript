1db7b7120197087dfd47ac9e0add76592f4b3064
Tagore9930 <tagore9930@gmail.com>
2025-12-20 21:47:59 +0530

    Completed Topic: forEach With Maps and Sets

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -129,6 +129,9 @@ console.log('tagore'.at(-1));
 
 */
 
+/*
+//////////////////////////////////////////
+// Looping Arrays: forEach
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for (const mov of movements) {
@@ -153,3 +156,45 @@ movements.forEach((mov, i, arr) => {
 // 1: function(450)
 // 2: function(-400)
 // ...
+
+*/
+
+//////////////////////////////////////////////
+// Sets practice with my brother.
+
+/*
+const A = new Set([1, 2, 3]);
+const B = new Set([3, 4, 5]);
+
+console.log(A);
+console.log(B);
+
+const a = A.union(B); // 3 - A U B
+const b = A.intersection(B); // 3 - A !U B
+const c = A.difference(B); // 1, 2 - A - B
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+*/
+
+//////////////////////////////////////////////
+// forEach With Maps and Sets
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach((value, key, map) => {
+  console.log(`${key}: ${value}`);
+});
+
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+
+console.log(currenciesUnique);
+currenciesUnique.forEach((value, _, set) => {
+  console.log(`${value}: ${value}`);
+});
