e7a71bcc74162bf8187bc9ace225b1a4ebb41d76
Tagore9930 <tagore9930@gmail.com>
2025-12-20 19:29:28 +0530

    Completed Topic: The New at Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,6 +75,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/*
+/////////////////////////////////////////////////
+// Simple Array Methods
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // SLICE - It will not modify the original
@@ -105,3 +108,20 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(' '));
+
+*/
+
+///////////////////////////////////////
+// The new at Method
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element types
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('tagore'.at(0));
+console.log('tagore'.at(-1));
