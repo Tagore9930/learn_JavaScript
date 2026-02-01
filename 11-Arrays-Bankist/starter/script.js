f13fdbee6096addaae8600468d3811bf3c2e581b
Tagore9930 <tagore9930@gmail.com>
2026-02-01 21:28:43 +0530

    Completed Challenge: Coding Challenge #3 in "Working with Arrays [Project]"

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -445,6 +445,7 @@ console.log(avg2);
 
 */
 
+/*
 // The Magic of Chaining Methods
 
 const euroToUsd = 1.1;
@@ -462,3 +463,28 @@ const totalDepositsUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 console.log(totalDepositsUSD);
+
+*/
+
+/* 
+// CHALLENGE #3
+
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = dogsAges =>
+  dogsAges
+    .map(dogsAge => (dogsAge <= 2 ? 2 * dogsAge : 16 + dogsAge * 4))
+    .filter(humanAge => humanAge >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1);
+console.log(avg2);
