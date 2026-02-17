f8799fd3d8d51310dbb75902d3dca242631b7fd6
Tagore9930 <tagore9930@gmail.com>
2026-02-18 00:27:43 +0530

    Completed Topic:  flat and flatMap

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -715,6 +715,9 @@ if (latestLargeMovement !== -1) {
 
 */
 
+/* //////////////////////////////////////////////
+// some and every methods
+
 console.log(movements);
 
 // EQUALITY:
@@ -743,3 +746,25 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+
+*/
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, [6]]], 7, 8];
+// console.log(arrDeep.flat(2));
+console.log(arrDeep.flat(3));
+
+// flat
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log('overalBalance:', overalBalance);
+
+// flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log('overalBalance with flatMap:', overalBalance);
