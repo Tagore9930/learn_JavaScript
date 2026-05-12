6b293e634a83c54fceaa1f3a9297c9411a4dfb31
Tagore9930 <tagore9930@gmail.com>
2026-05-12 23:22:43 +0530

    Completed Topic: Our First AJAX Call: XMLHttpRequest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,3 +10,36 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
+
+function getCountryData(country) {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    console.log(data, this);
+
+    const html = `
+        <article class="country">
+          <img class="country__img" src="${data?.flags?.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data?.name?.common}</h3>
+            <h4 class="country__region">${data?.region}</h4>
+            <p class="country__row"><span>👫</span>${(data?.population / 1000000).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data?.languages.eng}</p>
+            <p class="country__row"><span>💰</span>${data?.currencies?.INR?.name}</p>
+          </div>
+        </article>
+    `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = '1';
+  });
+}
+
+getCountryData('india');
+getCountryData('usa');
+getCountryData('pakistan');
