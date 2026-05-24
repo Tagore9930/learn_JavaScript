f76c71e92681871dbe6f9f1f2d95b248794f1dbf
Tagore9930 <tagore9930@gmail.com>
2026-05-24 22:27:49 +0530

    Completed Topic: Running Promises in Parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -26,6 +26,13 @@ const renderError = function (msg) {
   countriesContainer.style.opacity = '1';
 };
 
+const getJSON = function (url, errorMsg) {
+  return fetch(url).then(response => {
+    if (!response?.ok) throw new Error(errorMsg);
+    return response.json();
+  });
+};
+
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -142,12 +149,6 @@ console.log(request);
 // };
 
 /*
-const getJSON = function (url, errorMsg) {
-  return fetch(url).then(response => {
-    if (!response?.ok) throw new Error(errorMsg);
-    return response.json();
-  });
-};
 
 const getCountryData = function (country) {
   getJSON(
@@ -461,6 +462,7 @@ createImage('./img/img-1.jpg')
   .catch(err => console.log(err));
 */
 
+/* ///////////////////////////////////////////////
 // Consuming Promises with Async/Await
 
 const getPosition = function () {
@@ -525,3 +527,32 @@ console.log('1. Will get location.');
 
   console.log('3: Finished getting location.');
 })();
+*/
+
+// Running Promises in Parallel
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    // console.log([
+    //   data1?.capital?.[0],
+    //   data2?.capital?.[0],
+    //   data3?.capital?.[0],
+    // ]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    console.log(data);
+    console.log(data?.map(d => d[0]?.capital?.[0]));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('india', 'us', 'pakistan');
